63b696ee331289757a6026e6531d8224055ad2c9
ls <song.liu@ia.ac.cn>
2022-07-26 10:16:05 +0800

    update

--- InfoS.m
+++ InfoS.m
@@ -24,10 +24,7 @@ classdef InfoS < matlab.System
         is_first = 1;
         speed_list = [0 1 2 3 4 5 6 7 8 9 10 11 10 9 8 7 6 5 4 3 2 1 0 ...
             -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0];
-        torque_list = [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 ...
-            16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6 -7 ...
-            -8 -9 -10 -11 -12 -13 -14 -15 -16 -17 -16 -15 -14 -13 -12 -11 ...
-            -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0];
+        torque_list = [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17];
     end
 
     methods(Access = protected)
@@ -46,14 +43,15 @@ classdef InfoS < matlab.System
                     obj.torque_state = 1;
                 end
                 obj.hs = obj.hs + 1;
-                if t > (obj.pre_time+1)
+                if t > (obj.pre_time+4)
                     % switch
-                    if obj.torque_state == 69
+                    if obj.torque_state == 18
                         if obj.speed_state == 45
                             obj.speed_state = 45;
+                            obj.torque_state = 1;
                         else
                             obj.speed_state = obj.speed_state + 1;
-                            obj.torque_state = 0;
+                            obj.torque_state = 1;
                         end
                         obj.pre_time = t;
                     else
